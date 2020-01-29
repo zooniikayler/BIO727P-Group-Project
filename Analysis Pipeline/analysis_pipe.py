5384f38e37a0401f0e10fb89e29891b38002e5c8
Zoonii Kayler <zooniikayler@gmail.com>
2020-01-29 13:21:12 +0000

    updating github

--- Analysis Pipeline/analysis_pipe.py
+++ Analysis Pipeline/analysis_pipe.py
@@ -18,19 +18,23 @@ df_user['Position'] = ''
 df_user['Position'] = df_user.Substrate.str.split("(", n=1, expand = True)[1].str.replace(")", "")
 df_user['Substrate'] = df_user.Substrate.str.split("(", n=1, expand = True)[0]
 
+
+df_user = df_user.rename(columns={[1]: 'newName1', 'oldName2': 'newName2'})
+
 df_user['AZ20_fold_change_log2'] = np.log2(df_user['AZ20_fold_change'])
 
+
 #adds the kinase names to the dataframe with user's data if their is a matching SUB_MOD_RSD and SUB_GENE
-df_user = df_user.join(df_home[["KINASE", "SUB_GENE", "SUB_MOD_RSD"]].set_index(["SUB_GENE", "SUB_MOD_RSD"]),
+df_user = df_user.join(df_home[["KINASE_SYMBOL", "SUB_GENE", "SUB_MOD_RSD"]].set_index(["SUB_GENE", "SUB_MOD_RSD"]),
              on= ['Substrate', 'Position'])
 
-df_home = df_user.join(df_home[["KINASE", "SUB_ACC_ID", "SUB_MOD_RSD"]].set_index(["SUB_ACC_ID", "SUB_MOD_RSD"]),
+df_home = df_user.join(df_home[["KINASE_SYMBOL", "SUB_ACC_ID", "SUB_MOD_RSD"]].set_index(["SUB_ACC_ID", "SUB_MOD_RSD"]),
              on= ['Substrate', 'Position'], rsuffix= '_sub_acc')
 
 #cleaning data, removing Nans, O, infinity
 df_user = df_user.dropna()
 df_user= df_user.dropna(axis=0)
-df_user = df_user.dropna(subset = ['KINASE'])
+df_user = df_user.dropna(subset = ['KINASE_SYMBOL'])
 df_user = df_user[(df_user.AZ20_fold_change_log2 != "inf")]
 df_user = df_user[(df_user.AZ20_fold_change_log2 != "0")]
 
@@ -38,29 +42,29 @@ df_user = df_user[(df_user.AZ20_fold_change_log2 != "0")]
 def mean_score(kinase, dataset):
     '''mean of the log2 FC of all phosphosites in the substrate set'''
 
-    sub_set = dataset[dataset['KINASE'] == kinase]
-    return print(sub_set['AZ20_fold_change_log2'].mean())
+    sub_set = dataset[dataset['KINASE_SYMBOL'] == kinase]
+    return sub_set['AZ20_fold_change_log2'].mean()
 
 def alt_score(threshold, kinase, dataset):
     '''mean of the log2 FC for all SIGNIFICANT phosphosites in each substrate set'''
-    sub_set = dataset[(dataset['KINASE'] == kinase) & (dataset['AZ20_p-value'] < threshold)]
+    sub_set = dataset[(dataset['KINASE_SYMBOL'] == kinase) & (dataset['AZ20_p-value'] < threshold)]
     return print(sub_set['AZ20_fold_change_log2'].mean())
 
 def delta_score(threshold, kinase, dataset):
     '''number of significant positive phosphosite minus
     significant down-regulated sites in each substrate set'''
-    sub_set_neg = dataset[(dataset['KINASE'] == kinase) & (dataset['AZ20_p-value'] < threshold) &
+    sub_set_neg = dataset[(dataset['KINASE_SYMBOL'] == kinase) & (dataset['AZ20_p-value'] < threshold) &
                       (dataset['AZ20_fold_change_log2'] < 0)]
-    sub_set_pos = dataset[(dataset['KINASE'] == kinase) & (dataset['AZ20_p-value'] < threshold) &
+    sub_set_pos = dataset[(dataset['KINASE_SYMBOL'] == kinase) & (dataset['AZ20_p-value'] < threshold) &
                           (dataset['AZ20_fold_change_log2'] > 0)]
-    return print(len(sub_set_pos)-len(sub_set_neg))
+    return len(sub_set_pos)-len(sub_set_neg)
 
 
 
 
-mean_score("CDK1", df_user)
-alt_score(0.05, "CDK1", df_user)
-delta_score(0.05, "CDK1", df_user)
+#mean_score("CDK1", df_user)
+#alt_score(0.05, "CDK1", df_user)
+#delta_score(0.05, "CDK1", df_user)
 
 
 
@@ -79,20 +83,31 @@ volcano_plot(df_user)
 import plotly.express as px
 
 
-def kinase_barplot(user_data):
-    sub_set = user_data[(user_data['KINASE'])]
+#def kinase_barplot(user_data):
+y_plot = set(df_user.KINASE_SYMBOL)
+x_plot = []
 
-    x_plot = []
-    y_plot = []
-    for x in sub_set:
-        score1 = mean_score(x, user_data)
-        x_plot.append(x)
-        y_plot.append(score1)
-    kinase_activity = pd.DataFrame(columns=(['x_plot'], ['y_plot']))
-    fig = px.bar(kinase_activity, x=x_plot, y=y_plot)
-    fig.show()
+for k in y_plot:
+    x = mean_score(k, df_user)
+    x_plot.append(x)
 
-kinase_barplot(df_user)
+
+labels = ['Activity','Kinase']
+kin_activity = pd.DataFrame(columns=labels)
+kin_activity['Activity']=x_plot
+kin_activity['Kinase']=y_plot
+kin_activity.sort_values(['Activity'], ascending=[True])
+
+fig = px.bar(kin_activity, color='Activity',  y='Activity', x='Kinase')
+fig.update_layout(
+    title='Relative Kinase Activity' + " User Uploaded Data",
+    xaxis_tickfont_size=14,
+    yaxis=dict(
+        title='Activity',
+        titlefont_size=16,
+        tickfont_size=14))
+fig.update_layout(xaxis={'categoryorder':'total ascending'})
+fig.show()
 
 
 
