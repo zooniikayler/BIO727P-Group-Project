687923981d5ff98cec2d03b5613e02df9d97f647
Zoonii Kayler <zooniikayler@gmail.com>
2020-02-04 16:38:27 +0000

    Deletion

--- kinase_kin/main.py
+++ kinase_kin/main.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template, flash, redirect, request, url_for
+from .analysis_pipe import *
+
 
 #from forms import FileRequired
 import os
-
 from werkzeug.utils import secure_filename
 from wtforms import Form, StringField, SelectField, validators, SubmitField, FileField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileRequired, FileField, FileAllowed
-
-from db_setup import init_db, db_session
-from models import KinaseInfo, SubstrateInfo, InhibitorInfo
 
 
 class FileForm(Form):
@@ -17,9 +15,9 @@ class FileForm(Form):
     submit = SubmitField('Submit')
 
 class KinaseSearchForm(Form):
-	choices = [('Kinase Name', 'Kinase Name'), ('Uniprot Accession Number', 'Uniprot Accession Number')] # Define choices for the kinase search
-	select = SelectField('Search for Kinase:', choices=choices) 
-	search = StringField('',validators=[DataRequired()])
+	#choices = [('Kinase Symbol', 'Kinase Symbol')] # Define choices for the kinase search
+	search = StringField('Enter a Kinase Name', validators=[DataRequired()]) #Search Field will include choices defined
+	submit = SubmitField("Search")
 
 
 class InhibitorSearchForm(Form):
@@ -36,11 +34,7 @@ class phosphositesSearchForm(Form):
     choices = [('Phosphosite', 'Phosphosite')]
     select = SelectField(choices=choices)
     search = StringField('',[validators.DataRequired()])
-############################################################################################
 
-init_db() #initialise the db
-
-############################################################################################
 #instantiate flask app
 app = Flask(__name__)
 
@@ -59,62 +53,10 @@ def Home():
 
 @app.route('/Kinases', methods=['GET', 'POST'])
 def Kinases():
-	search = KinaseSearchForm(request.form) #request search from & run request
-	#may need to try submit as difference in forms
-	if request.method== 'POST': #if user posting search string to get info from db
-		return kinase_results(search)
-	return	render_template('Kinases.html', form=search)
-
-@app.route('/kinase_results')
-def kinase_results(search):
-	results = []
-	search_string = search.data['search'] #when given user input data
-	#search_string = search_string.upper()
-
-	if search_string:
-		if search.data['select']=='Kinase Name': #check if kinase symbol was selected
-			qry = db_session.query(KinaseInfo).filter(KinaseInfo.Kinase_Symbol.ilike(search_string))
-			results= qry.all() #output all query results
-
-			inhibitor_qry = db_session.query(KinaseInfo, InhibitorInfo)\
-					.filter(KinaseInfo.Kinase_Symbol.ilike(search_string))\
-					.join(InhibitorInfo, KinaseInfo.Kinase_Symbol== InhibitorInfo.Kinase_Target) #run join query to find all inhibitors with the corresponding kinase symbol target
-			inhibitor_results = inhibitor_qry.all()
-
-			substrate_qry = db_session.query(KinaseInfo, SubstrateInfo)\
-					.filter(KinaseInfo.Kinase_Symbol.ilike(search_string))\
-					.join(SubstrateInfo, KinaseInfo.Kinase_Symbol == SubstrateInfo.Kinase)
-			substrate_results = substrate_qry.all()
-
-		elif search.data['select'] == 'Uniprot Accession Number': #check Uniprot Accession Number search was selected
-			qry =db_session.query(KinaseInfo).filter(KinaseInfo.Uniprot_Accession_Number.contains(search_string)) #qry uniprot accession number 
-			results=qry.all()
-
-	if not results:
-		flash('No results found!') #flash error message
-		return redirect('/Kinases') #return back to kinase search
-
-	elif search.data['select'] == 'Uniprot Accession Number': # if user selected uniprot accession number
-		return render_template('uniprot.html', results=results)
-    
-    else:
-        #displaying results
-        return render_template('kinase_results.html', results=results, inhibitor_results = inhibitor_results, substrate_results)
-
-@app.route('/Kinases/<Kinase_Symbol>)
-def profile(Kinase_Symbol):
-    qry = db_session.query(KinaseInfo).filter(KinaseInfo.Kinase_Symbol.ilike(Kinase_Symbol))
-    results= qry.all()
-    
-    inhibitor_qry = db_session.query(KinaseInfo, InhibitorInfo)\
-            .join(InhibitorInfo, KinaseInfo.Kinase_Symbol== InhibitorInfo.Kinase_Target)
-    inhibitor_results = inhibitor_qry.all()
-      
-    substrate_qry = db_session.query(KinaseInfo, SubstrateInfo)\
-					.join(SubstrateInfo, KinaseInfo.Kinase_Symbol == SubstrateInfo.Kinase)
-    substrate_results = substrate_qry.all()
-    
-    return render_template('kinase_results.html', results=results, inhibitor_results = inhibitor_results, substrate_results)
+	form = KinaseSearchForm()
+	if form.validate():
+		return 'Form Successfully Submitted'
+	return	render_template('Kinases.html', form=form)
 
 ############################  About us   #########################################################
 
@@ -124,42 +66,43 @@ def About_us():
 
 ############################  Data Analysis   #########################################################
 
-# UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
-# ALLOWED_EXTENSIONS= {'csv', 'tsv', 'txt'}
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
+ALLOWED_EXTENSIONS= {'csv'}
 
-# def allowed_file(filename):
-#     return '.' in filename and \
-#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-# @app.route('/Data_Analysis', methods = ['GET', 'POST'])
-# def Upload():
-# 	form=FileForm()
-# 	if request.method == 'POST':
-# 		if request.files:
-# 			file = request.files['file']
-# 				#return redirect(request.url)
-# 			#if file.filename == '':
-# 				#flash('No file selected')
-# 				#return redirect(request.url)
-# 			if file and allowed_file(file.filename):
-
-# 				upload_directory = os.path.join(app.instance_path, 'uploaded_file')
-# 				if not os.path.exists(upload_directory):
-# 					os.makedirs(upload_directory)
-# 				file.save(os.path.join(upload_directory, secure_filename(file.filename)))
-# 				return redirect(url_for('Home'))
-# 	return	render_template('Data_Analysis.html', form=form)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-# @app.route('/results', methods=['GET', 'POST'])
-# def results():
-# 	return render_template("results.html")
+@app.route('/Data_Analysis', methods = ['GET', 'POST'])
+def Upload():
+	form=FileForm()
+	if request.method == 'POST':
+		if request.files:
+			file = request.files['file']
+				#return redirect(request.url)
+			#if file.filename == '':
+				#flash('No file selected')
+				#return redirect(request.url)
+			if file and allowed_file(file.filename):
+				upload_directory = os.path.join(app.instance_path)
+				if not os.path.exists(upload_directory):
+					os.makedirs(upload_directory)
+				file.save(os.path.join(upload_directory, secure_filename(file.filename)))
+				return redirect(url_for('results'))
+	return	render_template('Data_Analysis.html', form=form)
+
+
+@app.route('/results')
+def results():
+	y_axis = set(create_df_user('instance/sample_file.csv').Kinase)
+	x_axis = kinase_barplot_x(create_df_user('instance/sample_file.csv'))
+	barplot_dict = dict(y_axis, x_axis)
+
+	return render_template("results.html")
 
 
 
-
-#@app.route('/Upload/Save/Analysis', methods=['POST'])
 
 
 ############################  Inhibitors   ########################################################
